5300474a44eb7ad9e76e578fa54b2e1221ed0983
dvwpxr <132722469+dvwpxr@users.noreply.github.com>
2023-11-23 09:26:23 +0700

    Update Jawaban1.sql

--- Jawaban1.sql
+++ Jawaban1.sql
@@ -1,20 +1,26 @@
-BEGIN;
-INSERT INTO `tp_dosen` (`id_dosen`, `nama`, `NIDN`) VALUES (11, 'Bambang', 2022);
-INSERT INTO `tp_dosen` (`id_dosen`, `nama`, `NIDN`) VALUES (12, 'Budi', 2023);
-COMMIT;
+DROP TABLE IF EXISTS `tp_dosen`;
+CREATE TABLE `tp_dosen` (
+  `id_dosen` int(10) NOT NULL,
+  `nama` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `NIDN` smallint(10) DEFAULT NULL,
+  PRIMARY KEY (`id_dosen`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-SET FOREIGN_KEY_CHECKS = 1;
+CREATE TABLE `tr_matakuliah` (
+  `id_matakuliah` smallint(5) NOT NULL,
+  `kode` char(10) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `nama` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `dosen` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id_matakuliah`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-BEGIN;
-INSERT INTO `tp_mahasiswa` (`id_mahasiswa`, `nama`, `Fakultas`, `NIM`, `id_jurusan`, `id_matakuliah`, `id_dosen`) VALUES (22, 'Andi winato', 'MESIN', 2021, 14, 912, 12);
-INSERT INTO `tp_mahasiswa` (`id_mahasiswa`, `nama`, `Fakultas`, `NIM`, `id_jurusan`, `id_matakuliah`, `id_dosen`) VALUES (88, 'Dava Wisda Putra', 'FASILKOM', 2022, 12, 911, 11);
-COMMIT;
-
-SET FOREIGN_KEY_CHECKS = 1;
-
-BEGIN;
-INSERT INTO `tr_matakuliah` (`id_matakuliah`, `kode`, `nama`, `dosen`) VALUES (911, 'A', 'Basis Data', 'Jefry');
-INSERT INTO `tr_matakuliah` (`id_matakuliah`, `kode`, `nama`, `dosen`) VALUES (912, 'AB', 'RPL', 'Diah');
-COMMIT;
-
-SET FOREIGN_KEY_CHECKS = 1;
+CREATE TABLE `tp_mahasiswa` (
+  `id_mahasiswa` int(10) NOT NULL AUTO_INCREMENT,
+  `nama` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `Fakultas` varchar(10) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `NIM` int(10) NOT NULL,
+  `id_jurusan` smallint(1) DEFAULT NULL,
+  `id_matakuliah` smallint(1) DEFAULT NULL,
+  `id_dosen` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id_mahasiswa`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
